775e8988bf62bd3442e4f34d33398833c96dc907
Donour Sizemore <donour@cs.unm.edu>
2013-11-04 19:02:25 +0000

    added run script

--- transmitter.py
+++ transmitter.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 ###############################################################################
 """
 Shark transmission protocol implementation
@@ -7,19 +7,27 @@ __author__ = 'Donour Sizemore'
 
 import socket
 
+PROTOCOL_VERSION = "1.0"
+VERBOSITY = 2
+
 class Transmitter:
     def __init__(self, port = 55555):
         self.seqnum = 0
         self.port = port
         
     def send(self, host_list, timestamp, weight, height): 
-        msgstring = "--,SHARKNET1, %s, %d, %d, %.3f,--,--,--,--,--,--" %\
-                    (timestamp, self.seqnum, weight, height)
+        msgstring = "%s,SHARKNET1, %s, %d, %d, %.3f,--,--,--,--,--,--" %\
+                    (PROTOCOL_VERSION, timestamp, self.seqnum, weight, height)
         msg = bytes(msgstring,'UTF-8')
+
+        if VERBOSITY > 1:            
+            print(msgstring)
     
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         for addr in  host_list:
             sock.sendto(msg, (addr, self.port))
+            if VERBOSITY > 0:
+                print("sending to host: " + addr)
 
         self.seqnum = self.seqnum + 1
 
