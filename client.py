39b8f29022088cceaf09e1dbddda6df26c904304
Donour Sizemore <donour@cs.unm.edu>
2013-11-12 21:48:51 +0000

    working on client

--- client.py
+++ client.py
@@ -6,9 +6,12 @@ import SocketServer, socket
 PROTOCOL_VERSION="1.0"
 VERBOSITY = 99
 
+last_msg = ""
+
 class SharksDataHandler(SocketServer.BaseRequestHandler):
     def handle(self):
-        print self.client_address[0]
+        global last_msg
+        last_msg = self.request[0].strip()
 
 class recv_server:
 
@@ -22,13 +25,23 @@ class recv_server:
     def register(self, host):
         print "sending"
         self.reg_sock.sendto("HELO", (host,self.port+1))
-    
+
+    def get_sample(self):
+        self.sock.handle_request()
+        
 if __name__ == "__main__":
     host = "192.168.0.1"
     s = recv_server(host)
 
     import time
     while True:
+        freq = 100
+        for i in range(0,freq):
+            s.get_sample()
+            if last_msg is not None:
+                print last_msg
+                last_msg = None
+            time.sleep(1.0/freq)           
         s.register(host)
-        time.sleep(1)
+
     
